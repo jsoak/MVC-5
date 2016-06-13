90379a36c3ac1bbe9d27af964cd06381d2764e53
John Oak <joak@ctcd.edu>
2016-06-13 11:24:45 -0500

    Finished Chapter 3

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Text;
 using System.Web.Mvc;
 using LanguageFeatures.Models;
 
@@ -17,25 +18,206 @@ namespace LanguageFeatures.Controllers
             return "Navigate to a URL to show an example";
         }
 
-        public ViewResult AutoProperty()
+        public ViewResult CreateCollection()
         {
+            //A simple string array holding fruit names
+            string[] stringArray = { "apple", "orange", "plum" };
+            //Create a list holding the monetary values
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+            //Create a dictionary to tie the monetary values to the fruit names
+            //When I search for a key value of 10, I should easily find the associated value, which is apple
+            Dictionary<string, int> myDict = new Dictionary<string, int>
+            {
+                {"apple", 10 }, {"orange", 20 }, {"plum", 30 }
+            };
+
+            return View("Result", (object)stringArray[1]);
+        }
+
+        public ViewResult CreateProduct()
+        {
+
+            //Create a new Product object and set the property value using object intializer
+            Product myProduct = new Product
+            {
+                ProductID = 100,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Price = 275M,
+                Category = "Watersports"
+            };
+
+            /*Same as below except with using object initializer
             //create a new Product object
             Product myProduct = new Product();
 
             //set the property value
+            myProduct.ProductID = 100;
             myProduct.Name = "Kayak";
+            myProduct.Description = "A boat for one person";
+            myProduct.Price = 275M;
+            myProduct.Category = "Watersports";
 
             //get the property value
             string productName = myProduct.Name;
 
-            //generate the view
-            return View("Result", (object)String.Format("Product name: {0}", productName));
-        }
-        /*
         public ActionResult Index()
         {
             return View();
         }
         */
+            //generate the view
+            return View("Result", (object)String.Format("Category: {0}", myProduct.Category));
+        }
+
+        public ViewResult UseExtension()
+        {
+            //create and populate ShoppingCart
+            ShoppingCart cart = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name = "Kayak", Price = 275M },
+                    new Product {Name = "Lifejacket", Price = 48.95M },
+                    new Product {Name = "Soccer ball", Price = 19.50M },
+                    new Product {Name = "Corner flag", Price = 34.9M }
+                }
+
+            };
+            //get the total value of the products in the cart
+            decimal cartTotal = cart.TotalPrices();
+
+            return View("Result", (object)String.Format("Total: {0:c}", cartTotal));
+        }
+
+        public ViewResult UseExtensionEnumerable()
+        {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name = "Kayak", Price = 275M },
+                    new Product {Name = "Lifejacket", Price = 48.95M},
+                    new Product {Name = "Soccer ball", Price = 19.50M},
+                    new Product {Name = "Corner flag", Price = 34.95M}
+                }
+            };
+
+            //create and populate an array of Product objects
+            Product[] productArray =
+            {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+            };
+
+            //get the total value of the products in the cart
+            decimal cartTotal = products.TotalPrices();
+            decimal arrayTotal = products.TotalPrices();
+
+            return View("Result",
+                (object)String.Format("Cart Total: {0}, Array Total: {1}", cartTotal, arrayTotal));
+        }
+
+        public ViewResult UseFilterExtensionMethod()
+        {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+                    new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+                    new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+                    new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+                }
+            };
+            //Func<Product, bool> categoryFilter = prod => prod.Category == "Soccer";
+
+            /* Same as below without lambda expression
+            Func<Product, bool> categoryFilter = delegate (Product prod)
+            {
+                return prod.Category == "Soccer";
+            };
+            */
+
+            decimal total = 0;
+            //foreach (Product prod in products.Filter(categoryFilter))
+            foreach (Product prod in products.Filter(prod => prod.Category == "Soccer" || prod.Price > 20))
+            {
+                total += prod.Price;
+            }
+            return View("Result", (object)String.Format("Total: {0}", total));
+        }
+
+        public ViewResult CreateAnonArray()
+        {
+            var oddsAndEnds = new[] 
+            {
+                new { Name = "MVC", Category = "Pattern"},
+                new { Name = "Hat", Category = "Clothing"},
+                new { Name = "Apple", Category = "Fruit"}
+            };
+
+            StringBuilder result = new StringBuilder();
+            foreach (var item in oddsAndEnds)
+            {
+                result.Append(item.Name).Append(" ");
+            }
+
+            return View("Result", (object)result.ToString());
+
+        }
+
+        public ViewResult FindProducts()
+        {
+            Product[] products =
+            {
+                new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+                new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+                new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+                new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+            };
+
+            //Use LINQ to query data. Not sure if we ever need to use LINQ. We may use 
+            //Colleague transaction instead.
+
+            var foundProducts = from match in products
+                                orderby match.Price descending
+                                select new { match.Name, match.Price };
+
+            var results = products.Sum(e => e.Price);
+
+            products[2] = new Product { Name = "Stadium", Price = 79600M };
+            //Create the result
+            int count = 0;
+            StringBuilder result = new StringBuilder();
+            foreach (var p in foundProducts)
+            {
+                result.AppendFormat("Price: {0} ", p.Price);
+                if (++count == 3)
+                {
+                    break;
+                }
+            }
+
+            return View("Result", (object)result.ToString());
+
+            /*
+             *
+            //Define the array to hold the results
+            Product[] foundProducts = new Product[3];
+            //Sort the contents of the array
+            Array.Sort(products, (item1, item2) =>
+            {
+                return Comparer<decimal>.Default.Compare(item1.Price, item2.Price);
+            });
+            //get the first three items in the array as the results
+            Array.Copy(products, foundProducts, 3);
+ 
+             */
+        }
+
+
     }
 }
--- LanguageFeatures/LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -7,7 +7,26 @@ namespace LanguageFeatures.Models
 {
     public class Product
     {
+
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+
+        /*The top is the same as the following below with automatic property
         private string name;
+        private int productID;
+        private string description;
+        private decimal price;
+        private string category;
+
+        
+        public int ProductID
+        {
+            get { return productID; }
+            set { productID = value; }
+        }
 
         public string Name
         {
@@ -16,6 +35,13 @@ namespace LanguageFeatures.Models
 
         }
 
-        //Change
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+         */
+
+
     }
 }
