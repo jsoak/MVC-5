69574a8a25f7894113774157c036cfddb2153b96
John Oak <joak@ctcd.edu>
2016-06-13 16:03:48 -0500

    Sample Interface

---
+++ Sample Interfaces and Classes/Sample Interfaces and Classes/Cars.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_Interfaces_and_Classes
+{
+    class Cars
+    {
+        //Automatic Property Assignment
+        public int carWheels { get; set; }
+        public int carWindows { get; set; }
+        public string carColor { get; set; }
+        public int carPassengerSeats { get; set; }
+        public double carGasTankSize { get; set; }
+        public double carMaxDistanceOnFullTank { get; set; }
+    }
+}
