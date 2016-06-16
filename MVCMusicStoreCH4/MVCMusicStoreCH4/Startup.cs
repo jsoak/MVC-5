c97996299a29217be7a43d2081a5d451aae79572
John Oak <joak@ctcd.edu>
2016-06-16 17:29:17 -0500

    Start of Chapter 4

---
+++ MVCMusicStoreCH4/MVCMusicStoreCH4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCMusicStoreCH4.Startup))]
+namespace MVCMusicStoreCH4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
