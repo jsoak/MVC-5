13b77f44c13482afe412557fd58cb59e2bd58613
John Oak <joak@ctcd.edu>
2016-06-17 11:09:52 -0500

    Stopped at page 96

--- MVCMusicStoreCH4/MVCMusicStoreCH4/Global.asax.cs
+++ MVCMusicStoreCH4/MVCMusicStoreCH4/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MVCMusicStoreCH4.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,7 @@ namespace MVCMusicStoreCH4
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MusicStoreDbInitializer());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
