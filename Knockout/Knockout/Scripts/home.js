21eb45eaab5a1ad9dbe4b77ea9ba5f576805b24e
joak <kickenstein@hotmail.com>
2016-08-07 21:38:50 -0500

    Added Knockout Example

---
+++ Knockout/Knockout/Scripts/home.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    //$("#notwork").click(function () {
+    //    //$("#notwork").hide();
+    //    var sample = document.getElementById('realwork').innerHTML;
+    //    alert(sample);
+    //});
+
+    //Class Example
+    $(".pGroup").click(function () {
+        $(".pGroup").hide();
+    });
+});
